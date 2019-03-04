aee975d9e737c1be27420b41bb20d818b526d598
Pap <papwr17@gmail.com>
2019-03-03 19:55:58 -0600

    Added the remaining Custom Post Types to the Category Displays

--- Rocket Reference/functions.php
+++ Rocket Reference/functions.php
@@ -14,7 +14,7 @@ function theme_enqueue_styles() {
 function add_custom_post_types_cat($query) {
     if (is_category() || is_tag() && empty ($query->query_vars['suppress_filters'])) {
         $query->set ('post_type', array (
-            'post', 'nav_menu_item', 'engine'
+            'post', 'nav_menu_item', 'engine', 'launch_vehicle', 'stage', 'propellant'
         ));
     return $query;
     }
